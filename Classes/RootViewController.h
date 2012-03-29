a82398bb488975710f0ec44e27d9c3bd3f58d343
Shiny Yang <mootothemax@gmail.com>
2012-03-29 12:47:08 -0700

    initial commit, 3layer scrolling (FIX LOOPING)

---
+++ Classes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ios-test-1
+//
+//  Created by Student User on 3/27/12.
+//  Copyright University of Washington 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
